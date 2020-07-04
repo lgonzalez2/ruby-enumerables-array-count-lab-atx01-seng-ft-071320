466b60707e28864cc8af663765cef374c15cbb61
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-04 00:06:56 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,8 +4,11 @@ def count_strings(array)
   total = 0 
   
   array.count do |element|
-    if element.class 
+    if element.class == String 
+      total += 1 
+    end 
   end 
+  total
 end
 
 def count_empty_strings(array)
